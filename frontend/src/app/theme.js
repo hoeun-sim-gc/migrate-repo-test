c661d0980f42053ccd04ff7be0d2d53f4d3565c0
cxiao <chengyou.xiao@guycarp.com>
2021-10-16 00:51:14 -0400

    display in local time string

--- frontend/src/app/theme.js
+++ frontend/src/app/theme.js
@@ -14,8 +14,7 @@ export const darkTheme = createTheme({
 
 export const convertTime = (utc) => {
     try {
-        //force to conver to local string
-        var dt= new Date(Date.parse(utc)- new Date().getTimezoneOffset()* 60000); //fake time
+        var dt= new Date(Date.parse(utc)- new Date().getTimezoneOffset()* 60000*2); //fake time when convert to utc equals local
         return dt.toISOString().replace('T',' ').replace(/(\.[0-9]*Z)$/g, "");
     }
     catch (err){}
